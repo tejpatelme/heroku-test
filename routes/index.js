597be4dc7a2ac06314fbbd6d3b3d217c9b51def9
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-21 06:33:06 +0530

    add Re-exports file

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const { categoriesRouter } = require("./categories.router");
+const { productsRouter } = require("./products.router");
+const { cartRouter } = require("./cart.routes");
+
+module.exports = { categoriesRouter, productsRouter, cartRouter };
