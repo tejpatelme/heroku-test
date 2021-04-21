2e63981e498ab4ae734e81796f8dcd44894df0e8
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-21 06:35:01 +0530

    refactor : Cleaned up imports

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-
 const { connectToDatabase } = require("./routes/dbConnection");
-const { categoriesRouter } = require("./routes/categories.router");
-const { productsRouter } = require("./routes/products.router");
+const {
+  categoriesRouter,
+  productsRouter,
+  cartRouter,
+} = require("./routes/index");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 connectToDatabase();
 
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
-
 app.use("/categories", categoriesRouter);
 app.use("/products", productsRouter);
+app.use("/cart", cartRouter);
 
 app.get("/", (req, res) =>
   res.send("Hello World! This is a test of deploying express app on heroku")
+);
+
+app.use((req, res) =>
+  res.status(404).json({ success: false, message: "Error 404" })
 );
 
 app.listen(PORT, () => console.log("server running on port", PORT));
