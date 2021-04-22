bffb6fdf64ba7c7427c67638f25a7c49800a4a9c
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-22 19:03:59 +0530

    update Cart Schema

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,22 +3,36 @@ const express = require("express");
 const { Cart } = require("../schemas/cart.schema");
 const cartRouter = express.Router();
 
-cartRouter.route("/").get(async (req, res) => {
-  try {
-    const cart = await Cart.find({}).populate("products", "name price");
-    res.json({ cart });
-  } catch (error) {
-    res.status(500).json({ errorMessage: error.message });
-  }
-});
+cartRouter
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const cart = await Cart.find({}).populate("products");
+      res.json({ cart });
+    } catch (error) {
+      res.status(500).json({ errorMessage: error.message });
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const cart = await Cart.create({ products: [] });
+      res.json({ success: true, cart });
+    } catch (error) {
+      res.json({ success: false, error: error.message });
+    }
+  });
 
 cartRouter.route("/:cartId").get(async (req, res) => {
   const { cartId } = req.params;
   try {
-    const cart = await Cart.findById(cartId).populate({
-      path: "products",
-      select: "name price",
-    });
+    const cart = await Cart.findById(cartId).populate("products", "name");
+    // const cart = await Cart.findById(cartId).populate("products.productId");
+    // const cart = await Cart.findById(cartId).populate({
+    //   path: "products",
+    //   populate: {
+    //     path: "productId",
+    //   },
+    // });
     res.json(cart);
   } catch (error) {
     res.json({ error: error.message });
@@ -30,6 +44,10 @@ cartRouter.route("/:cartId/:productId").post(async (req, res) => {
   try {
     const newCart = await Cart.findById(cartId);
     newCart.products.push(mongoose.Types.ObjectId(productId));
+    // newCart.products.push({
+    //   productId: productId, //mongoose.Types.ObjectId(productId),
+    //   qty: 1,
+    // });
     newCart.save();
     res.json({ success: true, newCart });
   } catch (error) {
--- schemas/cart.schema.js
+++ schemas/cart.schema.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const CartSchema = new Schema({
+  // products: [
+  //   {
+  //     _id: false,
+  //     productId: { type: Schema.Types.ObjectId, ref: "Product" },
+  //     qty: Number,
+  //   },
+  // ],
   products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
 });
 
